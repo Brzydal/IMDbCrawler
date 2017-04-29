b395b3344b9e9baebe71ee0fda0bb51142b25357
Brzydal <damianfilipkowski@gmail.com>
2017-04-29 10:58:43 +0200

    Class Spider Ready with Tests

--- IMDbCrawler.py
+++ IMDbCrawler.py
@@ -2,52 +2,57 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Mother of spiders sent her kids to gather info and save it in a file
-def spiders_mum(url,how_many,file_name):
-    id_list = id_spider(url,how_many)
-    movies_dict = details_spider(id_list)
-    saving_spider(file_name,movies_dict)
 
-# Spider will get a list of movies from IMDb (how many first movies you want)
-def id_spider(url,how_many):
-    result = []
-    page_text = requests.get(url).text
-    soup = BeautifulSoup(page_text)
-    movies = soup.findAll('td',{'class':'titleColumn'})
+class Spider():
 
-    for movie in movies:
-        number = movie.text.split('.')[0]
-        links = movie.findAll('a')
-        for a in links:
-            href = a.get('href')
-            movie_id = parseing_spider(href)
-        if int(number) <= how_many:
-            result.append(movie_id)
+    # A Spider which will gather information about 'n' movies from website (IMDb) and save it in a file
 
-    return result
+    url = 'http://www.imdb.com/chart/top?ref=ft_250'
+    how_many_movies_ = 100
+    output_file_name = 'movies.csv'
+    list_of_id = []
+    movies_details = {}
 
-# This little parser will get a movie id from a href
+    def __init__(self,url,how_many_movies,output_file_name):
+        self.url = url
+        self.how_many_movies = how_many_movies
+        self.output_file_name = output_file_name
 
-def parseing_spider(href):
-    movie_id = href.split('/')[2]
-    return(movie_id)
+    def id_spider(self):
+        # This method will create a list of "n" movies id from IMDb
 
-# This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
+        page_text = requests.get(self.url).text
+        soup = BeautifulSoup(page_text)
+        movies = soup.findAll('td',{'class':'titleColumn'})
 
-def details_spider(id_list):
-    result = {}
-    for id in id_list:
-        response = requests.get('http://www.omdbapi.com/?i='+id)
-        result[response.json()['Title']] = response.json()['Year']
-    return result
+        for movie in movies:
+            number = movie.text.split('.')[0]
+            links = movie.findAll('a')
+            for a in links:
+                href = a.get('href')
+                movie_id = self.parseing_spider(href)
+            if int(number) <= self.how_many_movies:
+                self.list_of_id.append(movie_id)
 
-# This function will sort results and save to csv file
+    def details_spider(self):
+        # This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
 
-def saving_spider(file_name,movies_dict):
-    with open(file_name, 'w') as f:
-        w = csv.writer(f)
-        w.writerows(sorted(movies_dict.items(), key=lambda x: x[1]))
+        for movie_id in self.list_of_id:
+            response = requests.get('http://www.omdbapi.com/?i='+movie_id)
+            self.movies_details[response.json()['Title']] = response.json()['Year']
+
+    def saving_spider(self):
+        # This function will sort results and save to csv file
+        with open(self.output_file_name, 'w') as f:
+            w = csv.writer(f)
+            w.writerows(sorted(self.movies_details.items(), key=lambda x: x[1]))
+
+    @staticmethod
+    def parseing_spider(href):
+        # This little parser will get a movie id from a href
+
+        movie_id = href.split('/')[2]
+        return (movie_id)
 
 # I dont know if it was a part of the test but I have noticed that We could get title and year directly from td
 
-# Black_Widow = spiders_mum('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
--- test.py
+++ test.py
@@ -1,27 +1,41 @@
 from IMDbCrawler import *
 
-# Black_Widow = spiders_mum('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
+# Lets create our spider
+BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250',100,'movies.csv')
 
-# First lets test if we will get 100 movies
+# First lets test if parser is working fine
+def test_parsing_spider():
+    movie_id = BlackWidow.parseing_spider("/title/tt0068646/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=05HJ9PZ18AMRNFAJKAR0&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_2")
+    assert movie_id == 'tt0068646'
+
+# Lets create a list of 100 movie id
+BlackWidow.id_spider()
+
+# Lets test if we will get 100 movies
 def test_id_spider1():
-    assert len(id_spider('http://www.imdb.com/chart/top?ref=ft_250', 100)) == 100
+    assert len(BlackWidow.list_of_id) == 100
 
 # Then test if id of first movie is ok
 def test_id_spider2():
-    assert id_spider('http://www.imdb.com/chart/top?ref=ft_250', 100)[0] == 'tt0111161'
+    assert BlackWidow.list_of_id[0] == 'tt0111161'
 
 # Then test if id of last movie is ok
 def test_id_spider3():
-    assert id_spider('http://www.imdb.com/chart/top?ref=ft_250', 100)[99] == 'tt3783958'
+    assert BlackWidow.list_of_id[99] == 'tt3783958'
+
 
 # Then lets test if for id tt0068646 we will get Godfather with Year of 1972
 def test_details_spider1():
-    assert details_spider(['tt0068646']).keys()[0] == 'The Godfather'
-    assert details_spider(['tt0068646']).values()[0] == '1972'
+    BlackWidow.movies_details = {}
+    BlackWidow.list_of_id = ['tt0068646']
+    BlackWidow.details_spider()
+    assert BlackWidow.movies_details.keys()[0] == 'The Godfather'
+    assert BlackWidow.movies_details.values()[0] == '1972'
 
 # Then lets test if for id tt0208092 we will get Snatch with Year of 2000
 def test_details_spider2():
-    assert details_spider(['tt0208092']).keys()[0] == 'Snatch'
-    assert details_spider(['tt0208092']).values()[0] == '2000'
-
-
+    BlackWidow.movies_details = {}
+    BlackWidow.list_of_id = ['tt0208092']
+    BlackWidow.details_spider()
+    assert BlackWidow.movies_details.keys()[0] == 'Snatch'
+    assert BlackWidow.movies_details.values()[0] == '2000'
