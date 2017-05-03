317220197e836e5ce57e41f456e4f4ebcf530dc4
Brzydal <damianfilipkowski@gmail.com>
2017-05-03 23:50:08 +0200

    Comments in IMDbCrawler.py added

--- IMDbCrawler.py
+++ IMDbCrawler.py
@@ -2,18 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Mother of spiders sent her kids to gather info and save it in a file
-def spiders_mum(url,how_many,file_name):
-    id_list = id_spider(url,how_many)
-    movies_dict = details_spider(id_list)
-    saving_spider(file_name,movies_dict)
 
-# Spider will get a list of movies from IMDb (how many first movies you want)
-def id_spider(url,how_many):
+
+def spiders_mum(url, how_many, file_name):
+    """
+    Mother of spiders sent her kids to gather info and save it in a file
+
+    :param url:
+    :param how_many:
+    :param file_name:
+    """
+    id_list = id_spider(url, how_many)
+    movies_dict = details_spider(id_list)
+    saving_spider(file_name, movies_dict)
+
+
+
+def id_spider(url, how_many):
+
+    """
+    Spider will get a list of movies from IMDb (how many first movies you want)
+    :param url:
+    :param how_many:
+    :return: result - list of movies id
+    """
+
+    movie_id = None
     result = []
     page_text = requests.get(url).text
     soup = BeautifulSoup(page_text)
-    movies = soup.findAll('td',{'class':'titleColumn'})
+    movies = soup.findAll('td', {'class': 'titleColumn'})
 
     for movie in movies:
         number = movie.text.split('.')[0]
@@ -26,28 +44,51 @@ def id_spider(url,how_many):
 
     return result
 
-# This little parser will get a movie id from a href
+
+
 
 def parseing_spider(href):
-    movie_id = href.split('/')[2]
-    return(movie_id)
 
-# This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
+    """
+    # This little parser will get a movie id from a href
+    :param href:
+    :return: movie id
+    """
+    movie_id = href.split('/')[2]
+    return movie_id
+
+
 
 def details_spider(id_list):
+
+    """
+    This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
+    :param id_list:
+    :return: result - dictionary where titles are keys and Years of release are Values
+    """
+
     result = {}
     for id in id_list:
-        response = requests.get('http://www.omdbapi.com/?i='+id)
+        response = requests.get('http://www.omdbapi.com/?i=' + id)
         result[response.json()['Title']] = response.json()['Year']
     return result
 
-# This function will sort results and save to csv file
 
-def saving_spider(file_name,movies_dict):
+
+
+def saving_spider(file_name, movies_dict):
+
+    """
+    # This function will sort results and save to csv file
+    :param file_name:
+    :param movies_dict:
+    :return:
+    """
+
     with open(file_name, 'w') as f:
         w = csv.writer(f)
         w.writerows(sorted(movies_dict.items(), key=lambda x: x[1]))
 
-# I dont know if it was a part of the test but I have noticed that We could get title and year directly from td
+    
 
-# Black_Widow = spiders_mum('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
+        # Black_Widow = spiders_mum('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
