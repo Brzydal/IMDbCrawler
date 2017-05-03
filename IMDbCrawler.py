be0d891dc551f3cc3bcabf9382d33208ae29ae8f
Brzydal <damianfilipkowski@gmail.com>
2017-05-03 16:05:11 +0200

    test Changed to Class TestSpider

--- IMDbCrawler.py
+++ IMDbCrawler.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-class Spider():
 
-    # A Spider which will gather information about 'n' movies from website (IMDb) and save it in a file
+class Spider:
+
+    """
+    A Spider which will gather information about 'n' movies from website (IMDb) and save it in a file
+    """
 
     url = 'http://www.imdb.com/chart/top?ref=ft_250'
     how_many_movies_ = 100
@@ -15,51 +15,69 @@ class Spider():
     list_of_id = []
     movies_details = {}
 
-    def __init__(self,url,how_many_movies,output_file_name):
+    def __init__(self, url, how_many_movies, output_file_name):
         self.url = url
         self.how_many_movies = how_many_movies
         self.output_file_name = output_file_name
 
     def id_spider(self):
-        # This method will create a list of "n" movies id from IMDb
 
+        """
+        This method will create a list of "n" movies id from IMDb
+        """
+
+        movie_id = None
         page_text = requests.get(self.url).text
-        soup = BeautifulSoup(page_text)
-        movies = soup.findAll('td',{'class':'titleColumn'})
+        soup = BeautifulSoup(page_text, "lxml")
+        movies = soup.findAll('td', {'class': 'titleColumn'})
         for movie in movies:
             number = movie.text.split('.')[0]
             links = movie.findAll('a')
             for a in links:
                 href = a.get('href')
-                movie_id = self.parseing_spider(href)
+                movie_id = self.parsing_spider(href)
             if int(number) <= self.how_many_movies:
                 self.list_of_id.append(movie_id)
 
     def details_spider(self):
-        # This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
+
+        """
+        This spider will get a movies details by id from omdbapi and put them into dictionary {Title : Year}
+        """
 
         for movie_id in self.list_of_id:
-            response = requests.get('http://www.omdbapi.com/?i='+movie_id)
+            response = requests.get('http://www.omdbapi.com/?i=' + movie_id)
             self.movies_details[response.json()['Title']] = response.json()['Year']
 
     def saving_spider(self):
-        # This function will sort results and save to csv file
-        with open(self.output_file_name, 'wb') as f:
+
+        """
+        This function will sort results and save to csv file
+        :return a file with 100 movies
+        """
+
+        with open(self.output_file_name, 'w') as f:
             w = csv.writer(f)
-            w.writerows(sorted(self.movies_details.items(), key=lambda x: x[1]))
+            w.writerows(sorted(self.movies_details.items(), key=lambda x: x[1]))  # Sort by year, ascending
 
     @staticmethod
-    def parseing_spider(href):
-        # This little parser will get a movie id from a href
-
+    def parsing_spider(href):
+        #
+        """
+        This little parser will get a movie id from a href
+        :param href:
+        :return: movie_id
+        """
         movie_id = href.split('/')[2]
-        return (movie_id)
+        return movie_id
 
-# I dont know if it was a part of the test but I have noticed that We could get title and year directly from td
+if __name__ == "__main__":
+    print("### Starting Spider! ###")
+    BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
+    BlackWidow.id_spider()
+    print(len(BlackWidow.list_of_id))
+    BlackWidow.details_spider()
+    print(BlackWidow.movies_details)
+    BlackWidow.saving_spider()
+    print("### Spider has left... ###")
 
-# BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250',100,'movies.csv')
-# BlackWidow.id_spider()
-# print(len(BlackWidow.list_of_id))
-# BlackWidow.details_spider()
-# print(BlackWidow.movies_details)
-# BlackWidow.saving_spider()
--- test.py
+++ test.py
@@ -1,67 +1,78 @@
 from IMDbCrawler import *
 import os.path
 
-# Lets create our spider
-BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250',100,'movies.csv')
 
-# First lets test if parser is working fine
-def test_parsing_spider():
-    movie_id = BlackWidow.parseing_spider("/title/tt0068646/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=05HJ9PZ18AMRNFAJKAR0&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_2")
-    assert movie_id == 'tt0068646'
+class TestSpider:
 
-# Lets create a list of 100 movie id
-BlackWidow.id_spider()
+    # Lets create our spider
+    BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
 
-# Lets test if we will get 100 movies
-def test_id_spider1():
-    assert len(BlackWidow.list_of_id) == 100
+    def test_set_up(self):
+        self.BlackWidow = Spider('http://www.imdb.com/chart/top?ref=ft_250', 100, 'movies.csv')
+        self.BlackWidow.id_spider()
 
-# Then test if id of first movie is ok (the test will fail if ranking in IMDb will change)
-def test_id_spider2():
-    assert BlackWidow.list_of_id[0] == 'tt0111161'
-
-# Then test if id of last movie is ok (the test will fail if ranking in IMDb will change)
-def test_id_spider3():
-    assert BlackWidow.list_of_id[99] == 'tt0476735'
+    # First lets test if parser is working fine
+    def test_parsing_spider(self):
+        movie_id = self.BlackWidow.parsing_spider(
+            "/title/tt0068646/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=" +
+            "05HJ9PZ18AMRNFAJKAR0&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_2")
+        assert movie_id == 'tt0068646'
 
 
-# Then lets test if for id tt0068646 we will get Godfather with Year of 1972
-def test_details_spider1():
-    BlackWidow.movies_details = {}
-    BlackWidow.list_of_id = ['tt0068646']
-    BlackWidow.details_spider()
-    assert BlackWidow.movies_details.keys()[0] == 'The Godfather'
-    assert BlackWidow.movies_details.values()[0] == '1972'
-
-# Then lets test if for id tt0208092 we will get Snatch with Year of 2000
-def test_details_spider2():
-    BlackWidow.movies_details = {}
-    BlackWidow.list_of_id = ['tt0208092']
-    BlackWidow.details_spider()
-    assert BlackWidow.movies_details.keys()[0] == 'Snatch'
-    assert BlackWidow.movies_details.values()[0] == '2000'
-
-# Then lets test if saving spider created a file
-def test_saving_spider1():
-    assert os.path.exists('movies.csv')
-
-# Then lets test if for specific movie details saving spider will create file with three rows
-def test_saving_spider2():
-    BlackWidow.movies_details = {'Three':3,'One':1,'Two':2}
-    BlackWidow.saving_spider()
-    with open(BlackWidow.output_file_name, 'r') as f:
-        row_count = sum(1 for row in f)
-    assert row_count == 3
+    # Lets test if we will get 100 movies
+    def test_id_spider1(self):
+        assert len(self.BlackWidow.list_of_id) == 100
 
 
+    # Then test if id of first movie is ok (the test will fail if ranking in IMDb will change)
+    def test_id_spider2(self):
+        assert self.BlackWidow.list_of_id[0] == 'tt0111161'
 
-# And test if  saving spider will create file with 100 rows
-def test_saving_spider3():
-    BlackWidow.list_of_id = []
-    BlackWidow.id_spider()
-    BlackWidow.movies_details = {}
-    BlackWidow.details_spider()
-    BlackWidow.saving_spider()
-    with open(BlackWidow.output_file_name, 'r') as f:
-        row_count = sum(1 for row in f)
-    assert row_count == 100
+
+    # Then test if id of last movie is ok (the test will fail if ranking in IMDb will change)
+    def test_id_spider3(self):
+        assert self.BlackWidow.list_of_id[99] == 'tt0071853'
+
+
+    # Then lets test if for id tt0068646 we will get Godfather with Year of 1972
+    def test_details_spider1(self):
+        self.BlackWidow.movies_details = {}
+        self.BlackWidow.list_of_id = ['tt0068646']
+        self.BlackWidow.details_spider()
+        assert list(self.BlackWidow.movies_details.keys())[0] == 'The Godfather'
+        assert list(self.BlackWidow.movies_details.values())[0] == '1972'
+
+
+    # Then lets test if for id tt0208092 we will get Snatch with Year of 2000
+    def test_details_spider2(self):
+        self.BlackWidow.movies_details = {}
+        self.BlackWidow.list_of_id = ['tt0208092']
+        self.BlackWidow.details_spider()
+        assert list(self.BlackWidow.movies_details.keys())[0] == 'Snatch'
+        assert list(self.BlackWidow.movies_details.values())[0] == '2000'
+
+
+    # Then lets test if saving spider created a file
+    def test_saving_spider1(self):
+        assert os.path.exists('movies.csv')
+
+
+    # Then lets test if for specific movie details saving spider will create file with three rows
+    def test_saving_spider2(self):
+        self.BlackWidow.movies_details = {'Three': 3, 'One': 1, 'Two': 2}
+        self.BlackWidow.saving_spider()
+        with open(self.BlackWidow.output_file_name, 'r') as f:
+            row_count = sum(1 for row in f)
+        assert row_count == 3
+
+
+    # And test if  saving spider will create file with 100 rows
+    def test_saving_spider3(self):
+        self.BlackWidow.list_of_id = []
+        self.BlackWidow.id_spider()
+        self.BlackWidow.movies_details = {}
+        self.BlackWidow.details_spider()
+        self.BlackWidow.saving_spider()
+        with open(self.BlackWidow.output_file_name, 'r') as f:
+            row_count = sum(1 for row in f)
+        assert row_count == 100
